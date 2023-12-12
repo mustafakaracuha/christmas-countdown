335cf75c58e9cbf160b5a6893c12b28cb32b816f
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2023-12-12 12:47:29 +0300

    update

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -1,12 +1,12 @@
-import ReactAudioPlayer from "react-audio-player";
-import music from "../../assets/music.mp3";
+import ReactAudioPlayer from 'react-audio-player';
+import music from '../../assets/music.mp3';
 
 const MusicPlayer = () => {
   return (
     <>
       <hr className="mt-5 mb-5" />
       <ReactAudioPlayer
-        autoPlay
+        ref={audioRef}
         className="w-full"
         id="audioPlayer"
         src={music}
